376997b0cefbc362cfd593ed3fda1544b73dac8d
Peter Juras <me@peterjuras.com>
2018-06-10 14:03:54 +0200

    Output scripts if SLS_DEBUG is set

--- src/helpers/run-script.js
+++ src/helpers/run-script.js
@@ -1,10 +1,14 @@
-import { spawn } from 'child-process-promise';
-import path from 'path';
-import { FUNCTIONS_BASE_FOLDER } from './get-function-folders';
+import { spawn } from "child-process-promise";
+import path from "path";
+import { FUNCTIONS_BASE_FOLDER } from "./get-function-folders";
+
+const {
+  env: { SLS_DEBUG }
+} = process;
 
 export function runScript(script, folder) {
-  return spawn('yarn', [script], {
+  return spawn("yarn", [script], {
     cwd: path.join(FUNCTIONS_BASE_FOLDER, folder),
-    stdio: 'ignore'
+    stdio: SLS_DEBUG === "*" ? "inherit" : "ignore"
   });
 }
